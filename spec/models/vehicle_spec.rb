90619e917dc1be430df40979a26339e613db8d8e
Mike Kelley <mike@codezombie.org>
2024-04-22 13:40:02 -0700

    Added minimim price test

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -8,5 +8,12 @@ describe Vehicle do
       depreciation = Depreciation.new(subject.year, subject.purchased_at?)
       expect(subject.valuation).to eq(subject.msrp - depreciation.total)
     end
+
+    it "should never have a valuation less than MINIMUM_PRICE" do
+      depreciation = Depreciation.new(subject.year, subject.purchased_at?)
+      subject.update!(msrp: depreciation.total) # make it calculate to 0
+
+      expect(subject.valuation).to eq(described_class::MINIMUM_PRICE)
+    end
   end
 end
